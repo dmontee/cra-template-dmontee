bc82d9fd71c2173f15254b0f1ad445c91a20b315
Dalton Montee <daltonmontee@gmail.com>
2020-03-22 13:28:31 -0500

    Initial commit

---
+++ template/src/features/counter/reducer.js
@@ -0,0 +1,6 @@
+export default function reducer(state = 0, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
---
+++ template/src/features/index.js
@@ -0,0 +1,5 @@
+import counterReducer from "./counter/reducer";
+
+export default {
+    counter: counterReducer,
+};
